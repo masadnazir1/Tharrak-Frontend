86605efbf989d39cb258421fbbc5df882138ab68
M Asad Nazir <masadnazir1@gmail.com>
2025-03-27 01:39:35 +0500

    added the more app ui and logic

--- src/utils/EXPORTAPIURL.js
+++ src/utils/EXPORTAPIURL.js
@@ -1,6 +1,6 @@
-const BASE_URL = "http://localhost:5000/api"; // Replace with your actual API base URL
+//const BASE_URL = "http://localhost:5000/api"; // Replace with your actual API base URL
 //http://tharrak_api.galaxydev.pk/
-//const BASE_URL = "https://tharrakapi.galaxydev.pk/api";
+const BASE_URL = "https://tharrakapi.galaxydev.pk/api";
 
 const EXPORTAPIURL = {
   AUTH: {
